6ccf52706a1643df56ba002743cef8b335799a73
Rafia <rafiamaskeen@gmail.com>
2025-09-24 19:49:58 +0500

    DATA TYPES

--- test.js
+++ test.js
@@ -1,1 +1,34 @@
-console.log("Rafia Maskeen")
+// console.log("Rafia Maskeen")
+// const accountId = 144553
+// let accountEmail = "rafia@gmail.com"
+// var accountPassword = "12345"
+// accountCity ="Rajdhani"
+// let accountState;
+
+// accountId = 2  // Not Allowed
+// accountEmail ="rafi@gamil.com"
+// accountCity ="Kotli"
+
+// console.log(accountId)
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// ############## DATA TYPES ##########################
+"use strict";
+// alert(3 + 3)  // we are using node.js not browser
+let name = "Rafia"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => tru/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
